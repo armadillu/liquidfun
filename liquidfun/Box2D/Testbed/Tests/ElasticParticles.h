28d990b0b622fc30e6e82ff01299920220dada7e
Oriol Ferrer Mesia <oriol@localprojects.net>
2015-06-29 18:36:21 -0400

    tesging rings

--- liquidfun/Box2D/Testbed/Tests/ElasticParticles.h
+++ liquidfun/Box2D/Testbed/Tests/ElasticParticles.h
@@ -78,29 +78,94 @@ public:
 		}
 
 		{
-			b2CircleShape shape;
-			shape.m_p.Set(-1, 3);
-			shape.m_radius = 0.5f;
-			b2ParticleGroupDef pd;
-			pd.flags = b2_elasticParticle;
-			pd.groupFlags = b2_solidParticleGroup;
-			pd.shape = &shape;
-			pd.color.Set(0, 255, 0, 255);
-			m_particleSystem->CreateParticleGroup(pd);
+//			b2CircleShape shape;
+//			shape.m_p.Set(-1, 3);
+//			shape.m_radius = 0.5f;
+//			b2ParticleGroupDef pd;
+//			pd.flags = b2_elasticParticle;
+//			pd.groupFlags = b2_solidParticleGroup;
+//			pd.shape = &shape;
+//			pd.color.Set(0, 255, 0, 255);
+//			m_particleSystem->CreateParticleGroup(pd);
 		}
 
 		{
-			b2PolygonShape shape;
-			shape.SetAsBox(1, 0.5f);
+//			b2PolygonShape shape;
+//			shape.SetAsBox(1, 0.5f);
+//			b2ParticleGroupDef pd;
+//			pd.flags = b2_elasticParticle;
+//			pd.groupFlags = b2_solidParticleGroup;
+//			pd.position.Set(1, 4);
+//			pd.angle = -0.5f;
+//			pd.angularVelocity = 2.0f;
+//			pd.shape = &shape;
+//			pd.color.Set(0, 0, 255, 255);
+//			m_particleSystem->CreateParticleGroup(pd);
+		}
+
+		for(int k = 0; k < 2; k++){ //oriol testing rings
+
+			static const int32 shapeCount = 12;
+			b2PolygonShape polyShapes[shapeCount];
+			const b2Shape *shapes[shapeCount];
+			float shapeH = 0.1;
+			float separation = 0.5;
+			float radius = 0.4;
+			float TO_RAD = M_PI / 180.0f;
+			float angleStep = 360 / shapeCount;
+			for(int i = 0; i < shapeCount; i++){
+				float angA = i * angleStep;
+				float angB = (i + 1) * angleStep;
+				b2Vec2 pa;
+				pa.Set(radius * cos(angA * TO_RAD), radius * sin(angA * TO_RAD));
+
+				b2Vec2 pb;
+				pb.Set(radius * cos(angB * TO_RAD), radius * sin(angB * TO_RAD));
+
+				b2Vec2 vs[4];
+				vs[0].Set(0.0f + i * separation, 0.0f + k );
+				vs[1].Set(1.0f + i * separation, 0.0f + k);
+				vs[2].Set(1.0f + i * separation, shapeH + k);
+				vs[3].Set(0.0f + i * separation, shapeH + k);
+				polyShapes[i].Set(vs, 4);
+				shapes[i] = &polyShapes[i];
+			}
+
 			b2ParticleGroupDef pd;
-			pd.flags = b2_elasticParticle;
+			pd.flags = b2_elasticParticle | b2_springParticle;
 			pd.groupFlags = b2_solidParticleGroup;
 			pd.position.Set(1, 4);
-			pd.angle = -0.5f;
-			pd.angularVelocity = 2.0f;
-			pd.shape = &shape;
-			pd.color.Set(0, 0, 255, 255);
-			m_particleSystem->CreateParticleGroup(pd);
+			pd.angle = -0.0f;
+			pd.angularVelocity = 0.0f;
+			pd.shapes = shapes;
+			pd.shapeCount = shapeCount;
+			pd.stride = 0.055;
+			pd.color.Set(0, 255, 255, 255);
+			b2ParticleGroup* g = m_particleSystem->CreateParticleGroup(pd);
+
+			b2Vec2 force;
+			force.Set(-0.1,0.2);
+			g->ApplyLinearImpulse(force);
+		}
+
+
+		{ //oriol testing rings
+//			b2ChainShape shape;
+//			b2Vec2 vs[4];
+//			vs[0].Set(0.0f, 0.0f);
+//			vs[1].Set(1.0f, 0.0f);
+//			vs[2].Set(1.0f, 1.0f);
+//			vs[3].Set(0.0f, 1.0f);
+//			shape.CreateLoop(vs, 4);
+//			b2ParticleGroupDef pd;
+//			pd.flags = b2_elasticParticle;
+//			pd.groupFlags = b2_solidParticleGroup;
+//			pd.position.Set(1, 4);
+//			pd.angle = -0.0f;
+//			pd.angularVelocity = 0.0f;
+//			pd.shape = &shape;
+//			pd.color.Set(0, 255, 255, 255);
+//			m_particleSystem->CreateParticleGroup(pd);
 		}
 
 		{
